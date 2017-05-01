ddf7bca70d424e32cee1ca3d275f42e835b49a4e
Prathamesh Chavan <pc44800@gmail.com>
2017-05-01 22:41:39 +0530

    first-and-second-focus: Changed 'Count' to 'Relative Count'
    Changed 'Count' to 'Relative Count' and also did the same correction
    the plots which were created with the code which was modified.
    This addresses issue#3
    
    Signed-off-by: Prathamesh Chavan <pc44800@gmail.com>

--- first-and-second-focus.R
+++ first-and-second-focus.R
@@ -5,14 +5,14 @@ first_data <- read.csv('first-focus.csv')
 ggplot(first_data, aes(x=data.circle, y="", fill=data.circle))+
   geom_bar(width = 1, stat = "identity") +
   ggtitle("Bar-Plot for First-focus with\nrespect to Location") +
-  labs(x="Location",y="Count") +
+  labs(x="Location",y="Relative Count") +
   theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=32, hjust=0))+
   theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=22))+
   scale_fill_manual(values=c("#0000FF", "#00ff00", "#FF4500", "#ff0000", "#ffff00", "#ffff00"))
 ggplot(first_data, aes(x=data.color, y="", fill=data.color))+
   geom_bar(width = 1, stat = "identity") +
   ggtitle("Bar-Plot for First-focus with\nrespect to Color") +
-  labs(x="Color",y="Count") +
+  labs(x="Color",y="Relative Count") +
   theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=32, hjust=0))+
   theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=22))+
   scale_fill_manual(values=c("#0000FF", "#00ff00", "#FF4500", "#ff0000", "#ffff00", "#ffff00"))
@@ -21,14 +21,14 @@ second_data <- read.csv('second-focus.csv')
 ggplot(second_data, aes(x=data.circle, y="", fill=data.circle))+
   geom_bar(width = 1, stat = "identity") +
   ggtitle("Bar-Plot for Second-focus with\nrespect to Location") +
-  labs(x="Location",y="Count") +
+  labs(x="Location",y="Relative Count") +
   theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=32, hjust=0))+
   theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=22))+
   scale_fill_manual(values=c("#0000FF", "#00ff00", "#FF4500", "#ff0000", "#ffff00", "#ffff00"))
 ggplot(second_data, aes(x=data.color, y="", fill=data.color))+
   geom_bar(width = 1, stat = "identity") +
   ggtitle("Bar-Plot for Second-focus with respect to Color") +
-  labs(x="Color",y="Count") +
+  labs(x="Color",y="Relative Count") +
   theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=32, hjust=0))+
   theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=22))+
   scale_fill_manual(values=c("#0000FF", "#00ff00", "#FF4500", "#ff0000", "#ffff00", "#ffff00"))
